609bd3d96f4e395e451b18b27392e9bf59256e65
Lilqee29 <iqoyum29@gmail.com>
2026-08-25 13:04:14 +0200

    feat: setup EAS Build configuration and dynamic app.config.js for iOS widget support

---
+++ app.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  expo: {
+    name: 'Tally',
+    slug: 'tally-app',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'dark',
+    splash: {
+      image: './assets/splash-icon.png',
+      resizeMode: 'contain',
+      backgroundColor: '#000000',
+    },
+    ios: {
+      supportsTablet: false,
+      bundleIdentifier: 'com.qomex.tally',
+      deploymentTarget: '17.0',
+      entitlements: {
+        'com.apple.security.application-groups': ['group.com.qomex.tally'],
+      },
+      infoPlist: {
+        UIBackgroundModes: [],
+      },
+    },
+    plugins: [
+      [
+        '@bacons/apple-targets',
+        {
+          appleTeamId: process.env.EXPO_APPLE_TEAM_ID || process.env.APPLE_TEAM_ID || 'AAAAAAAAAA',
+        },
+      ],
+    ],
+    assetBundlePatterns: ['**/*'],
+    newArchEnabled: false,
+  },
+};
